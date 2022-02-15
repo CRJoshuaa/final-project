5a10feb831199be3eda4478c913316188c2ce416
Joshuaa <joshuaa.chales@gmail.com>
2022-02-15 10:34:33 +0800

    init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgMHG6C4PyAX1rxlQazbSzA86wLc7pS24",
+  authDomain: "besquare-final-project.firebaseapp.com",
+  projectId: "besquare-final-project",
+  storageBucket: "besquare-final-project.appspot.com",
+  messagingSenderId: "175419693435",
+  appId: "1:175419693435:web:c20b3168d257ca1d5545de",
+};
