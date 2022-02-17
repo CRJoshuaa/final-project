c5b2ac7b46a1658f5a0f6ecc65bd253a51c2a51a
Haziqah <your@email.comname>
2022-02-17 17:25:02 +0800

    added base messaging components

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,14 +1,39 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import "./ChatInput.css";
+import { db } from "../firebase";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 function ChatInput({ channelName, channelId }) {
+  const [input, setInput] = useState("");
+
   const sendMessage = (e) => {
     e.preventDefault(); //Prevents refresh
+
+    if (!channelId) {
+      return false;
+    }
+
+    db.collection("rooms").doc(channelId).collection("messages").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: "bruh",
+      userImage: "https://c.tenor.com/46lAWM-p0eYAAAAC/kermit-falling.gif",
+    });
+
+    setInput("");
   };
   return (
     <div className="chat-input">
       <form>
-        <input placeholder={`Message #ROOM`} />
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={`Message #${channelName}`}
+        />
         <Button hidden type="submit" onClick={sendMessage}>
           SEND
         </Button>
