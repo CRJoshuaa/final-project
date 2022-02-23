f5bca0ed0dc2dac63b1f44a8c3663675de27a7e7
Pravin030399 <irpravin@gmail.com>
2022-02-23 07:54:27 +0000

    final-project

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,13 +21,13 @@ import {
   Timeline,
   MonetizationOn,
   Home,
+  Chat,
 } from "@mui/icons-material";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import CryptoNews from "./CryptoNews";
 import { useNavigate } from "react-router-dom";
-import { useHistory } from "react-router-dom";
 import { color } from "@mui/system";
 
 function Sidebar() {
