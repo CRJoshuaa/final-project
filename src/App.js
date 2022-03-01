76c46c13948c5241171274e8bef13bf3612ddf57
Haziqah <your@email.comname>
2022-03-01 14:07:26 +0800

    styled the crypto pages

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           <>
             {/* A <Switch> looks through its children <Route>s and
            renders the first one that matches the current URL. */}
-            <Header />
+            {/* <Header /> */}
 
             <div className="app-body">
               <Sidebar />
