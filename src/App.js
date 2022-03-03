f2622112d413d32804907e7292587239cfacd2f8
Haziqah <your@email.comname>
2022-03-03 12:28:34 +0800

    added off white colour & used it for app body

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Header";
 import { auth } from "./firebase";
 import Login from "./components/Login";
 import Spinner from "react-spinkit";
