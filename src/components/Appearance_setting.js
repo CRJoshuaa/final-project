71663322779cf4858cb44fe1281404b1ed47eb52
Haziqah <your@email.comname>
2022-03-07 14:22:27 +0800

    made settings page responsive

--- src/components/Appearance_setting.js
+++ src/components/Appearance_setting.js
@@ -27,78 +27,77 @@ function AppearanceSetting() {
       <h2 className={`head ${darkMode ? "head-dark" : "head-light"}`}>
         Appearance
       </h2>
-      <div className={`para ${darkMode ? "para-dark" : "para-light"}`}>
-        <div className="chat-preview">
-          <h3> Chatroom Preview</h3>
-          <div className="preview-msg">
-            <img
-              className="avatar"
-              onClick={() => auth.signOut()}
-              alt={user?.displayName}
-              src={user?.photoURL}
-            />
-            <div
-              className={`msg-detail ${darkMode ? "msg-dark" : "msg-light"}`}
-            >
-              <div className="current-msg">
-                <h4>
-                  {user?.displayName}
-                  <span>27/7/1987, 12:00:00 PM</span>
-                </h4>
+      <div className="appearance-setting">
+        <div className={`para ${darkMode ? "para-dark" : "para-light"}`}>
+          <div className="chat-preview">
+            <h3> Chatroom Preview</h3>
+            <div className="preview-msg">
+              <img
+                className="avatar"
+                alt={user?.displayName}
+                src={user?.photoURL}
+              />
+              <div
+                className={`msg-detail ${darkMode ? "msg-dark" : "msg-light"}`}
+              >
+                <div className="current-msg">
+                  <h4>
+                    {user?.displayName}
+                    <span>27/7/1987, 12:00:00 PM</span>
+                  </h4>
 
-                <p>Never gonna let you give you up</p>
-                <ReplyIcon />
+                  <p>Never gonna let you give you up</p>
+                  <ReplyIcon />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="preview-msg">
-            <img
-              className="avatar"
-              onClick={() => auth.signOut()}
-              alt={user?.displayName}
-              src={user?.photoURL}
-            />
-            <div
-              className={`msg-detail ${darkMode ? "msg-dark" : "msg-light"}`}
-            >
-              <div className="current-msg">
-                <h4>
-                  {user?.displayName}
-                  <span>27/7/1987, 12:00:00 PM</span>
-                </h4>
-                <p>Never gonna let you down</p>
-                <ReplyIcon />
+            <div className="preview-msg">
+              <img
+                className="avatar"
+                alt={user?.displayName}
+                src={user?.photoURL}
+              />
+              <div
+                className={`msg-detail ${darkMode ? "msg-dark" : "msg-light"}`}
+              >
+                <div className="current-msg">
+                  <h4>
+                    {user?.displayName}
+                    <span>27/7/1987, 12:00:00 PM</span>
+                  </h4>
+                  <p>Never gonna let you down</p>
+                  <ReplyIcon />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="preview-user">
-            <img
-              className="user-avatar"
-              onClick={() => auth.signOut()}
-              alt={user?.displayName}
-              src={user?.photoURL}
-            />
-            <div
-              className={`user-msg ${darkMode ? "user-dark" : "user-light"}`}
-            >
-              <div className="current-msg">
-                <h4>
-                  {user?.displayName}
-                  <span>27/7/1987, 12:00:00 PM</span>
-                </h4>
-                <p>Never gonna run around and desert you</p>
-                <ReplyIcon />
+            <div className="preview-user">
+              <img
+                className="user-avatar"
+                alt={user?.displayName}
+                src={user?.photoURL}
+              />
+              <div
+                className={`user-msg ${darkMode ? "user-dark" : "user-light"}`}
+              >
+                <div className="current-msg">
+                  <h4>
+                    {user?.displayName}
+                    <span>27/7/1987, 12:00:00 PM</span>
+                  </h4>
+                  <p>Never gonna run around and desert you</p>
+                  <ReplyIcon />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <button
+          className={`btn ${darkMode ? "btn-dark" : "btn-light"}`}
+          onClick={changeTheme}
+        >
+          {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
       </div>
-      <button
-        className={`btn ${darkMode ? "btn-dark" : "btn-light"}`}
-        onClick={changeTheme}
-      >
-        {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
-      </button>
     </div>
   );
 }
