2505d2c425ce5714f42ec3be03e4e071c3da6376
Joshuaa <joshuaa.chales@gmail.com>
2022-03-10 17:06:09 +0800

    login page update and responsive

--- src/components/LoginBackUp.js
+++ src/components/LoginBackUp.js
@@ -4,6 +4,9 @@ import "./LoginBackUp.css";
 import { auth, provider } from "../firebase";
 import { FcGoogle } from "react-icons/fc";
 import logo from "../images/appLogoSquare.png";
+import RedditIcon from "@mui/icons-material/Reddit";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function LoginBackUp() {
   const signIn = (e) => {
@@ -12,7 +15,23 @@ function LoginBackUp() {
   };
   return (
     <div className="login">
-      <div className="login-header"></div>
+      <div className="login-header">
+        <a
+          className="social-icon"
+          href="https://www.linkedin.com/showcase/besquarebyderiv/"
+        >
+          <LinkedInIcon />
+        </a>
+        <a href="https://twitter.com/BeSquareByDeriv" className="social-icon">
+          <TwitterIcon />
+        </a>
+        <a
+          href="https://www.reddit.com/user/Deriv_official/"
+          className="social-icon"
+        >
+          <RedditIcon />
+        </a>
+      </div>
       <div className="login-body">
         <div className="login-left">
           <img
